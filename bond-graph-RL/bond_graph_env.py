2141d22686baf81bf50a84f7bca6f58c0b915025
Daniel Nguyen <danieltvnguyen@gmail.com>
2024-03-04 22:14:27 -0500

    requirements

--- bond-graph-RL/bond_graph_env.py
+++ bond-graph-RL/bond_graph_env.py
@@ -4,33 +4,27 @@ import gymnasium as gym
 from gymnasium import spaces
 import math
 from bond_graph import *
+from bond_graph_nodes import*
 from itertools import permutations
 import random
 
 class BondGraphEnv(gym.Env):
     def __init__(self, max_bond_graphs, max_nodes):
+        super(BondGraphEnv, self).__init__()
+        
+        
+        
         num_node_types = len(BondGraphElementTypes)
         
         num_edge_actions = math.permute(max_nodes, 2)*2 # Multiply by 2 for bond causality
         num_node_actions = num_node_types**max_nodes # Max number of nodes allowed in bg times number of node types
         
         self.possible_edge_list = permutations(range(max_nodes), 2)
-        
-        # self.edge_action_space = spaces.Discrete(num_edge_actions)
-        # self.node_action_space = spaces.Discrete(num_node_actions)
-        
-        ## Actions
-        # Add node
-        # Connect two existing nodes
-        
+
         self.bond_graph_space = spaces.Discrete(max_bond_graphs)
         self.bond_graph_list = []
     
-        # self.node_type_space = spaces.Discrete(num_node_types)
-        # self.edge_space = spaces.MultiDiscrete([max_nodes, max_nodes]) 
-        # self.edge_type_space = spaces.Discrete(2) # 2 different types of causality
-        
-        
+
         add_node_or_edge = spaces.Discrete(2) # 0 for add node, 1 for add edge
         node_space = spaces.Discrete(num_node_types)        
         edge_space = spaces.MultiDiscrete([max_nodes, max_nodes, 2]) 
@@ -49,14 +43,6 @@ class BondGraphEnv(gym.Env):
             return
         pass
     
-    def _get_action_mask(self, add_node_or_edge):
-        if add_node_or_edge == 0:
-            
-            return 
-        else:
-            return
-            
-        return 
     
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
