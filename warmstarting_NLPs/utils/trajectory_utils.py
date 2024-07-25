a277910720e2a86bcf299bbaf260648b42c3f5c6
Daniel Nguyen <danieltvnguyen@gmail.com>
2024-07-25 17:35:27 -0400

    visualization of warmstart trajectories

--- warmstarting_NLPs/utils/trajectory_utils.py
+++ warmstarting_NLPs/utils/trajectory_utils.py
@@ -7,15 +7,16 @@ from collections import namedtuple
 #         pass
 
 #     create_idx(nx ,)
+
 def create_idx(nx,nu,N):
     nz = (N-1) * nu + N * nx
-    x = [i * (nx + nu) + np.arange(nx) for i in range(N)]
-    u = [i * (nx + nu) + nx + np.arange(nu) for i in range(N-1)]
+    X = [i * (nx + nu) + np.arange(nx) for i in range(N)]
+    U = [i * (nx + nu) + nx + np.arange(nu) for i in range(N-1)]
 
     nc = (N-1) * nx
     c = [i * nx + np.arange(nx) for i in range(N-1)]
 
-    return namedtuple('idx', ['nx', 'nu', 'N', 'nz', 'nc', 'x', 'u', 'c'])(nx=nx, nu=nu, N=N, nz=nz, nc=nc, x=x, u=u, c=c)
+    return namedtuple('idx', ['nx', 'nu', 'N', 'nz', 'nc', 'X', 'U', 'c'])(nx=nx, nu=nu, N=N, nz=nz, nc=nc, X=X, U=U, c=c)
 
 # np.arange(8)
 # nx=3
